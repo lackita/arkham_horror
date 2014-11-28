118943c05fad64e32811bba5f604058cb7c2269c
Colin Williams <lackita@gmail.com>
2014-11-27 21:19:45 -0500

    setup begin is more elaborate

--- src/arkham_horror/setup.clj
+++ src/arkham_horror/setup.clj
@@ -1,11 +1,13 @@
 (ns arkham-horror.setup
   (:require [arkham-horror.game :as game]
             [arkham-horror.ancient-one :as ancient-one]
-            [arkham-horror.combat :as combat]))
+            [arkham-horror.combat :as combat]
+            [arkham-horror.investigator :as investigator]))
 
 (defn begin [ancient-one investigators]
   (ancient-one/awaken (game/make {:ancient-one ancient-one
-                                  :investigators investigators})))
+                                  :investigators (map #(investigator/make (:name %) (:stats %))
+                                                      investigators)})))
 
 (defn onslaught [active-game]
   (if (game/over? active-game)
--- test/arkham_horror/setup_test.clj
+++ test/arkham_horror/setup_test.clj
@@ -2,11 +2,15 @@
   (:require [clojure.test :refer :all]
             [arkham-horror.setup :as setup]
             [arkham-horror.ancient-one :as ancient-one]
-            [arkham-horror.game :as game]))
+            [arkham-horror.game :as game]
+            [arkham-horror.investigator :as investigator]))
 
 (deftest begin-test
   (is (ancient-one/awakened? (setup/begin :cthulu [])))
-  (is (ancient-one/valid? (setup/begin :azathoth []))))
+  (is (ancient-one/valid? (setup/begin :azathoth [])))
+  (is (= (:investigators (setup/begin :cthulu [{:name "Monterey Jack"
+                                                :stats {:speed 2 :fight 2 :lore 2}}]))
+         [(investigator/make :monterey-jack {:speed 2 :fight 2 :lore  2})])))
 
 (deftest setup-test
   (is (game/over? (setup/onslaught (setup/begin :cthulu [])))))
