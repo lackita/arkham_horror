e4d5f59596ecb109388bc19dd068bbe50a7204a4
Colin Williams <lackita@gmail.com>
2014-11-30 09:09:59 -0500

    Fixed some of the user stuff

--- src/user.clj
+++ src/user.clj
@@ -4,13 +4,13 @@
 
 (def active-game (agent nil))
 
-(defmacro make-facade [function]
+(defn make-facade [function]
   (fn [& args]
     (send active-game function args)
     (await active-game)
     (setup/game-status @active-game)))
 
-(def begin (make-facade (fn [_] (setup/begin ancient-one investigators))))
+(def begin (make-facade setup/begin))
 (def init (make-facade setup/init))
 (def awaken (make-facade setup/awaken))
 (def attack (make-facade setup/attack))
