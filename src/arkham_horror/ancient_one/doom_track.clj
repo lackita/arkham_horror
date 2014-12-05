bc5be63b3e8340e24b2d5227dc805df71a26e3e9
Colin Williams <lackita@gmail.com>
2014-12-04 21:38:49 -0500

    doom track move

--- src/arkham_horror/ancient_one/doom_track.clj
+++ src/arkham_horror/ancient_one/doom_track.clj
@@ -18,16 +18,18 @@
 (defn level [doom-track]
   (doom-track :level))
 
-(defn move [game direction bound]
-  (if (= (level (-> game :ancient-one :doom-track)) bound)
-    game
-    (update-in game [:ancient-one :doom-track :level] direction)))
+(defn move [doom-track direction bound]
+  (if (= (level doom-track) bound)
+    doom-track
+    (update-in doom-track [:level] direction)))
 
 (defn advance [game]
-  (move game inc (capacity (-> game :ancient-one :doom-track))))
+  (update-in game [:ancient-one :doom-track]
+             #(move % inc (capacity %))))
 
 (defn retract [game]
-  (move game dec 0))
+  (update-in game [:ancient-one :doom-track]
+             #(move % dec 0)))
 
 (defn fill [doom-track]
   (assoc doom-track :level (capacity doom-track)))
