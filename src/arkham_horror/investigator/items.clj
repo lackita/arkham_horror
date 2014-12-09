c1712a9db8daa23830cfb3a84f583a1a8ea6af2f
Colin Williams <lackita@gmail.com>
2014-12-08 19:24:57 -0500

    Removed some complexity

--- src/arkham_horror/investigator/items.clj
+++ src/arkham_horror/investigator/items.clj
@@ -10,24 +10,3 @@
 
 (defn refresh [items]
   (map #(assoc % :exhausted false) items))
-
-(defn unexhausted-named [items name]
-  (filter #(and (= name (:name %)) (not (:exhausted %))) items))
-
-(defn exhauster [item]
-  #(dice/update % dice/reroll-lowest))
-
-(defn exhaust-first [items pred]
-  {:items (loop [processed []
-                 current (first items)
-                 remaining (rest items)]
-            (cond (and (pred current) (not (:exhausted current)))
-                    (concat processed [(assoc current :exhausted true)] remaining)
-                  (empty? remaining)
-                    (throw (Throwable. "No items found"))
-                  :else
-                    (recur (conj processed current) (first remaining) (rest remaining))))
-   :change #(dice/update % dice/reroll-lowest)})
-
-(defn exhaust-first-named [items name]
-  (:items (exhaust-first items #(= (:name %) name))))
