ba02b7bafd3ac3b7663da36f97beca8011caf31d
Colin Williams <lackita@gmail.com>
2014-12-17 09:28:58 -0500

    Removed spurious help

--- src/arkham_horror/game/active.clj
+++ src/arkham_horror/game/active.clj
@@ -19,13 +19,11 @@
 (defn help []
   @help-info)
 
-(defn pre-begin [& _]
-  {})
 (defn reset []
   (set-help! '[(begin <config>)])
   (if (agent-error active-game)
-    (restart-agent active-game (pre-begin) :clear-actions true)
-    (send active-game pre-begin)))
+    (restart-agent active-game {} :clear-actions true)
+    (send active-game (fn [_] {}))))
 
 (reset)
 
@@ -33,56 +31,69 @@
   (fn [& args]
     (apply send active-game function args)
     (await active-game)
-    (print (game/message @active-game))
-    (help)))
+    (print (game/message @active-game))))
 
 (defn begin [config]
   ((make-facade (fn [_] (game/make config))))
   (set-help! '[(start-init)]))
+
 (defn start-init []
   ((make-facade phase/start-init))
   (set-help! '[(init-investigator {:speed <speed> :fight <fight> :lore <lore>})]))
+
 (defn init-investigator [config]
   ((make-facade #(game/init-investigator % config)))
   (set-help! '[(advance-phase)]))
+
 (defn advance-phase []
   ((make-facade game/advance-phase))
   (await active-game)
   (set-help! `[(~(@active-game :end-phase))]))
+
 (defn end-init []
   ((make-facade phase/end-init))
   (set-help! '[(awaken)]))
+
 (defn awaken []
   ((make-facade ancient-one/awaken))
   (await active-game)
   (if (game/over? @active-game)
     (set-help! '[(reset)])
     (set-help! '[(start-upkeep)])))
+
 (defn start-upkeep []
   ((make-facade phase/start-upkeep))
   (set-help! '[(focus-investigator {:speed-sneak <delta> :fight-will <delta> :lore-luck <delta>})
                (advance-phase)]))
+
 (defn focus-investigator [deltas]
   ((make-facade #(game/focus-investigator % deltas))))
+
 (defn end-upkeep []
   ((make-facade phase/end-upkeep))
   (set-help! '[(start-attack)]))
+
 (defn start-attack []
   ((make-facade combat/start))
   (set-help! '[(attack)]))
+
 (defn attack []
   (send dice #(dice/roll-and-save % 3))
   (structure/get-path @active-game [phase investigator])
   ((make-facade combat/investigator-attack))
   (set-help! '[(accept-roll) (exhaust-item <n>)]))
+
 (defn exhaust-item [n]
   ((make-facade #(setup/exhaust-item % n))))
+
 (defn accept-roll []
   ((make-facade combat/accept-roll))
   (set-help! '[(end-attack)]))
+
 (defn end-attack []
   ((make-facade combat/end))
   (set-help! '[(defend)]))
+
 (defn defend []
   ((make-facade combat/ancient-one-attack))
   (await active-game)
