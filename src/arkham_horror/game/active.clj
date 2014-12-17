f48170a5a3596c5d4a8cccb0b7236998e0f67eb9
Colin Williams <lackita@gmail.com>
2014-12-17 00:53:03 -0500

    Help text in active game

--- src/arkham_horror/game/active.clj
+++ src/arkham_horror/game/active.clj
@@ -11,20 +11,23 @@
 
 (def active-game (agent nil))
 (def dice (agent (dice/make :random)))
-(def help (ref (help/make)))
+(def help-info (ref nil))
+
+(defn set-help! [actions]
+  (dosync (ref-set help-info actions)))
+
+(defn help []
+  @help-info)
 
 (defn pre-begin [& _]
   (help/set-available-actions {} '[(begin <config>)]))
 (defn reset []
+  (set-help! '[(begin <config>)])
   (if (agent-error active-game)
     (restart-agent active-game (pre-begin) :clear-actions true)
     (send active-game pre-begin)))
 
 (reset)
-
-(defn help []
-  (await active-game)
-  (help/get-available-actions @active-game))
 
 (defn make-facade [function]
   (fn [& args]
@@ -36,34 +39,54 @@
     (help)))
 
 (defn begin [config]
-  ((make-facade (fn [_] (game/make config)))))
+  ((make-facade (fn [_] (game/make config))))
+  (set-help! '[(start-init)]))
 (defn start-init []
-  ((make-facade phase/start-init)))
+  ((make-facade phase/start-init))
+  (set-help! '[(init-investigator {:speed <speed> :fight <fight> :lore <lore>})]))
 (defn init-investigator [config]
-  ((make-facade #(game/init-investigator % config))))
+  ((make-facade #(game/init-investigator % config)))
+  (set-help! '[(advance-phase)]))
 (defn advance-phase []
-  ((make-facade game/advance-phase)))
+  ((make-facade game/advance-phase))
+  (await active-game)
+  (set-help! `[(~(@active-game :end-phase))]))
 (defn end-init []
-  ((make-facade phase/end-init)))
+  ((make-facade phase/end-init))
+  (set-help! '[(awaken)]))
 (defn awaken []
-  ((make-facade ancient-one/awaken)))
+  ((make-facade ancient-one/awaken))
+  (await active-game)
+  (if (game/over? @active-game)
+    (set-help! '[(reset)])
+    (set-help! '[(start-upkeep)])))
 (defn start-upkeep []
-  ((make-facade phase/start-upkeep)))
+  ((make-facade phase/start-upkeep))
+  (set-help! '[(focus-investigator {:speed-sneak <delta> :fight-will <delta> :lore-luck <delta>})
+               (advance-phase)]))
 (defn focus-investigator [deltas]
   ((make-facade #(game/focus-investigator % deltas))))
 (defn end-upkeep []
-  ((make-facade phase/end-upkeep)))
+  ((make-facade phase/end-upkeep))
+  (set-help! '[(start-attack)]))
 (defn start-attack []
-  ((make-facade combat/start)))
+  ((make-facade combat/start))
+  (set-help! '[(attack)]))
 (defn attack []
   (send dice #(dice/roll-and-save % 3))
   (structure/get-path @active-game [phase investigator])
-  ((make-facade combat/investigator-attack)))
+  ((make-facade combat/investigator-attack))
+  (set-help! '[(accept-roll) (exhaust-item <n>)]))
 (defn exhaust-item [n]
   ((make-facade #(setup/exhaust-item % n))))
 (defn accept-roll []
-  ((make-facade combat/accept-roll)))
+  ((make-facade combat/accept-roll))
+  (set-help! '[(end-attack)]))
 (defn end-attack []
-  ((make-facade combat/end)))
+  ((make-facade combat/end))
+  (set-help! '[(defend)]))
 (defn defend []
-  ((make-facade combat/ancient-one-attack)))
+  ((make-facade combat/ancient-one-attack))
+  (await active-game)
+  (when (game/over? @active-game)
+    (set-help! '[(reset)])))
