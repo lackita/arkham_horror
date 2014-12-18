fbf801f3e32ba2189b0d0dcbb88cb413d2ff200a
Colin Williams <lackita@gmail.com>
2014-12-18 07:51:58 -0500

    Extracted some duplication

--- src/arkham_horror/game/active.clj
+++ src/arkham_horror/game/active.clj
@@ -7,12 +7,10 @@
             [arkham-horror.phase :as phase]
             [arkham-horror.structure :as structure]
             [arkham-horror.investigator :as investigator]
-            [arkham-horror.investigator.dice :as old-dice]
-            [arkham-horror.dice :as dice]
+            [arkham-horror.investigator.dice :as dice]
             [clojure.string :refer [join]]))
 
 (def active-game (agent {}))
-(def dice (agent (dice/make :random)))
 (def help-info (ref nil))
 
 (defn set-help! [actions]
@@ -52,11 +50,6 @@
              '[(init-investigator {:speed <speed> :fight <fight> :lore <lore>})]
              "Initialization started"))
 
-(defn init-investigator [config]
-  (make-move #(game/init-investigator % config)
-             '[(advance-phase)]
-             (investigator/describe (structure/get-path @active-game [phase investigator]))))
-
 (defn advance-phase []
   (make-move game/advance-phase `[(~(@active-game :end-phase))]
              (if (phase/over? (phase/get @active-game)) "Phase over" "")))
@@ -68,49 +61,47 @@
   (make-move ancient-one/awaken '[(start-upkeep)]
              (str (:name (ancient-one/get @active-game)) " awakened")))
 
+(defn investigator-status []
+  (investigator/describe (structure/get-path @active-game [phase investigator])))
+
+(defn init-investigator [config]
+  (make-move #(game/init-investigator % config) '[(advance-phase)] (investigator-status)))
+
 (defn start-upkeep []
   (make-move phase/start-upkeep
              '[(focus-investigator {:speed-sneak <delta> :fight-will <delta> :lore-luck <delta>})
                (advance-phase)]
-             (investigator/describe (structure/get-path @active-game [phase investigator]))))
+             (investigator-status)))
 
 (defn focus-investigator [deltas]
-  (make-move #(game/focus-investigator % deltas) '[(advance-phase)]
-             (investigator/describe (structure/get-path @active-game [phase investigator]))))
+  (make-move #(game/focus-investigator % deltas) '[(advance-phase)] (investigator-status)))
 
 (defn end-upkeep []
   (make-move phase/end-upkeep '[(start-attack)]
              "Investigators refreshed"))
 
-(defn start-attack []
-  (make-move combat/start '[(attack)]
-             (str "Attack\nDoom track: " (doom-track/level
-                                          (structure/get-path @active-game
-                                                              [ancient-one doom-track])))))
-
-
-;; (send dice #(dice/roll-and-save % 3))
-;; (structure/get-path @active-game [phase investigator])
+(defn roll-status []
+  (join " " (cons "Roll:"
+                  (sort (dice/pending-roll
+                         (structure/get-path @active-game
+                                             [phase investigator dice]))))))
 
 (defn attack []
-  (make-move combat/investigator-attack '[(accept-roll) (exhaust-item <n>)]
-             (join " " (cons "Roll:"
-                             (sort (old-dice/pending-roll
-                                    (structure/get-path @active-game
-                                                        [phase investigator old-dice])))))))
+  (make-move combat/investigator-attack '[(accept-roll) (exhaust-item <n>)] (roll-status)))
 
 (defn exhaust-item [n]
-  (make-move #(setup/exhaust-item % n) '[(accept-roll)]
-             (join " " (cons "Roll:"
-                             (sort (old-dice/pending-roll
-                                    (structure/get-path @active-game
-                                                        [phase investigator old-dice])))))))
+  (make-move #(setup/exhaust-item % n) '[(accept-roll)] (roll-status)))
+
+(defn ancient-one-status []
+  (str "Attack\nDoom track: " (doom-track/level
+                               (structure/get-path @active-game
+                                                   [ancient-one doom-track]))))
+
+(defn start-attack []
+  (make-move combat/start '[(attack)] (ancient-one-status)))
 
 (defn accept-roll []
-  (make-move combat/accept-roll '[(end-attack)]
-             (str "Attack\nDoom track: " (doom-track/level
-                                          (structure/get-path @active-game
-                                                              [ancient-one doom-track])))))
+  (make-move combat/accept-roll '[(end-attack)] (ancient-one-status)))
 
 (defn end-attack []
   (make-move combat/end '[(defend)] "Defend"))
