d7124d64c04cb15e6cfc530901ee82bccb738f00
Colin Williams <lackita@gmail.com>
2014-12-17 22:53:22 -0500

    Reduced setup imports

--- src/arkham_horror/setup.clj
+++ src/arkham_horror/setup.clj
@@ -1,10 +1,6 @@
 (ns arkham-horror.setup
   (:require [arkham-horror.phase :as phase]
-            [arkham-horror.ancient-one :as ancient-one]
-            [arkham-horror.combat :as combat]
             [arkham-horror.investigator :as investigator]
-            [arkham-horror.investigator.dice :as dice]
-            [arkham-horror.ancient-one.doom-track :as doom-track]
             [arkham-horror.structure :as structure]))
 
 (defn focus [{investigators :investigators :as active-game} deltas]
